cde9962fbdfc451814b4365db8d3774859a7fdd5
Anton-Bodnar-Y <anton.bodnar.y@gmail.com>
2018-01-20 11:04:39 +0200

    worked with the main page. Go to the news page. Meta tags description for news 2

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -16,29 +16,54 @@
 
 @section('content')
 		
-		
-		<div id="news">
-			<div class="new">
+		<div id="content" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			
+			
+			
+			<div class="new col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-lg-5 col-md-5 col-sm-5 col-xs-5">
 					
 					
-					<?php //var_dump($new) ?>
+					
 					@foreach($new as $val)
-						<h2>{{ $val['news_title'] }}</h2>
-						<p>{{ date('l dS \o\f F Y h:i:s A', $val['date']) }}</p>
-						<p>{{ $val['img_1'] }}</p>
-						{!! $val['text'] !!}
-						<p>{{ $val['img_2'] }}</p>
-						<p>{{ $val['img_3'] }}</p>
+						<h3 class="title">{{ $val['news_title'] }}</h3>
+						<p class="date">{{ date('j-m-o, G:i', $val['date']) }}</p>
+						<p class="top_img"><img src="../../resources/views/img/{{ $val['img_1'] }}" alt=""/></p>
+						<div class="text_article">{!! $val['text'] !!}</div>
+						<div class="block_img">
+							<img src="../../resources/views/img/{{ $val['img_2'] }}" alt=""/>
+							<img src="../../resources/views/img/{{ $val['img_3'] }}" alt=""/>
+						</div>
+						
 						
 					@endforeach
-			
-			
+					
+					
+								
 			</div>
 			
 			
 			
+			<div class="otherNews col-lg-offset-2 col-md-offset-2 col-sm-offset-2 col-xs-offset-2 col-lg-5 col-md-5 col-sm-5 col-xs-5">
+			
+				<h3 class="title">Читайте также:</h3>
+				
+				<div class="otherNew col-lg-6 col-md-6 col-sm-6 col-xs-6">
+					<a href=""><h4 class="title">Название статьи</h4></a>
+					<a href=""><img src="../../resources/views/img/{{ $val['img_2'] }}" alt=""/></a>
+				</div>
+				
+				<div class="otherNew col-lg-6 col-md-6 col-sm-6 col-xs-6">
+					<a href=""><h4 class="title">Название статьи</h4></a>
+					<a href=""><img src="../../resources/views/img/{{ $val['img_2'] }}" alt=""/></a>
+				</div>
+				
+			</div>
+			
+			
+			
+			
+			
 		</div>
-		
 		
 	
 @endsection
